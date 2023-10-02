77fb4a0eb56a08a90bd2eb5ad304e65411f74dec
CicekBOT <146765089+CicekBOT@users.noreply.github.com>
2023-10-02 21:12:58 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,17 +4,17 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the CicekBOT for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳CicekBOT'u herkes kullanabilir ama eğer ki 13 yaşının altında olduğunuz tespit edilirse banlanırsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Botu kötü amaçlar için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳Bu botu herhangi birine pazarlıyamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Botun açıklarını herhangi bir şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳Bot ile troll yapamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
+<p>⊳Belirli komutları kullanmak için <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> sitesine oy vermelisiniz.</p>
 </div>
     </div>
   );
